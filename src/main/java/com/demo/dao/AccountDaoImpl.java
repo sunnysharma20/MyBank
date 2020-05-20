ffa92befa992129e7e288b6e49bc0fb687420874
pb <pb@y.c>
2020-05-20 20:56:20 +0530

    up

--- src/main/java/com/demo/dao/AccountDaoImpl.java
+++ src/main/java/com/demo/dao/AccountDaoImpl.java
@@ -44,7 +44,7 @@ public class AccountDaoImpl extends JdbcDaoSupport implements AccountDao {
 	@Override
 	public void insertAccount(Account account) {
 		String sql = "INSERT INTO account " +
-				"(name,address,balance) VALUES ( ?, ?, 0)" ;
+				"(name,address) VALUES ( ?, ?)" ;
 		getJdbcTemplate().update(sql, new Object[]{
 				account.getId(), account.getName()
 		});
