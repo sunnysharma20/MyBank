1625da12f8c82872e07b26e89350f70014f32db8
pb <pb@y.c>
2020-05-20 17:01:32 +0530

    updated

---
+++ src/main/java/com/demo/dao/EmployeeDao.java
@@ -0,0 +1,11 @@
+package com.demo.dao;
+
+import java.util.List;
+
+import com.demo.model.Employee;
+
+public interface EmployeeDao {
+	List<Employee> getAllEmployees();
+
+	void insertEmployee(Employee employee);
+}
